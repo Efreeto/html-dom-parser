7535078cbdd5aa9eb6d8ba7eda5bb112960fdf5b
Woo Kim <woowon.kim@gmail.com>
2015-02-11 11:51:04 -0800

    Update example.js

--- example.js
+++ example.js
@@ -2,7 +2,7 @@
 var server = require('webserver').create();
 
 // start a server on port 8080 and register a request listener
-server.listen(80, function(request, response) {
+server.listen(8080, function(request, response) {
 
     response.statusCode = 200;
   response.write('<html><body>Hello!</body></html>');
