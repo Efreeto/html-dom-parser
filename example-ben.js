6d9d60acf2e95c317aac15166bb210703665bc26
Woo Kim <woowon.kim@gmail.com>
2015-02-11 12:12:14 -0800

    Third

---
+++ example-ben.js
@@ -0,0 +1,36 @@
+// import the webserver module, and create a server
+var server = require('webserver').create();
+
+// start a server on port 8080 and register a request listener
+server.listen(8080, function(request, response) {
+
+  var page = new WebPage();
+
+  page.open("http://lanyrd.com/places/oxfordshire/", function(){
+    var events = page.evaluate(function(){
+      return $('.vevent .summary').map(function(e){ 
+        return '* ' + this.innerText
+      }).toArray().join('\n');
+    });
+
+    // Rather than console logging, write the data back as a 
+    // response to the user
+    //
+    // console.log('Upcoming Events in Oxfordshire:');
+    // console.log(events);
+
+    response.statusCode = 200;
+    response.write('Upcoming Events in Oxfordshire:\n');
+    response.write(events);
+    response.close();
+
+    // We want to keep phantom open for more requests, so we
+    // don't exit the process. Instead we close the page to
+    // free the associated memory heap
+    //
+    // phantom.exit();
+
+    page.close();
+
+  });
+});
