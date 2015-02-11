8aa5cab78c96eda3d52ce9b19f1befa7c3c91ab7
Woo Kim <woowon.kim@gmail.com>
2015-02-11 12:00:54 -0800

    Second

--- example.js
+++ example.js
@@ -1,10 +1,7 @@
-// import the webserver module, and create a server
-var server = require('webserver').create();
-
-// start a server on port 8080 and register a request listener
-server.listen(80, function(request, response) {
-
-    response.statusCode = 200;
+var webserver = require('webserver');
+var server = webserver.create();
+var service = server.listen(8080, function(request, response) {
+  response.statusCode = 200;
   response.write('<html><body>Hello!</body></html>');
   response.close();
 });
